da49b9a7cd3da98f736b569461c9b8063c1e67b1
Fernando Nieto <lestairon@Fernandos-MacBook-Pro.local>
2021-11-10 02:22:12 -0500

    allow users to sign_in

---
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  module V1
+    module Users
+      class SessionsController < DeviseTokenAuth::SessionsController
+        respond_to :json
+
+        protected
+
+        def render_create_success
+          render json: { message: 'Logged in successfully.' }, status: :created
+        end
+
+        def respond_to_on_destroy
+          head :ok
+        end
+      end
+    end
+  end
+end
