eba6bcfef9b67c3bf9b5932854941eb3220b1490
Fernando Nieto <fernandonietop9@gmail.com>
2021-12-20 14:55:34 -0500

    feature/allow user to recover the password (#15)
    * allow user to recover the password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_072849) do
+ActiveRecord::Schema.define(version: 2021_12_03_084210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,8 @@ ActiveRecord::Schema.define(version: 2021_12_03_072849) do
     t.string "unconfirmed_email"
     t.integer "targets_count"
     t.integer "gender"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.index ["email", "provider"], name: "index_users_on_email_and_provider", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
---
+++ spec/requests/api/v1/users/recover_password_spec.rb
@@ -0,0 +1,45 @@
+RSpec.describe 'Password reset', type: :request do
+  let(:user) { create(:user) }
+  let(:mails) { ActionMailer::Base.deliveries }
+  let(:reset_params) { {} }
+
+  subject { post api_v1_user_password_path, as: :json, params: reset_params }
+
+  context 'when trying to recover an existing user' do
+    let(:reset_params) { { email: user.email, redirect_url: '/' } }
+
+    it 'returns ok' do
+      subject
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'sends an email' do
+      expect { subject }.to change { mails.size }.by(1)
+    end
+
+    it 'sends the email to the email provided' do
+      subject
+
+      expect(mails.first.to.first).to eq(user.email)
+    end
+
+    it 'creates a new reset_password_token for user' do
+      expect { subject }.to change { user.reload.reset_password_token }
+    end
+  end
+
+  context 'when trying to recover non existing user' do
+    let(:reset_params) { { email: Faker::Internet.email, redirect_url: '/' } }
+
+    it 'returns not found' do
+      subject
+
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it 'doesnt send any email' do
+      expect { subject }.to_not change { mails.size }
+    end
+  end
+end
